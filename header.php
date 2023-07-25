4bd279be5ea97125f97ae4cc42999184ff5110b7
Rafael Gomes <rafael.degomes@gmail.com>
2023-07-25 16:40:34 -0300

    primeira modificacao no layout do login

---
+++ header.php
@@ -0,0 +1,100 @@
+ 
+                <footer class="footer">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <script>document.write(new Date().getFullYear())</script> © Reactly.
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="text-sm-end d-none d-sm-block">
+                                    Crafted with <i class="mdi mdi-heart text-danger"></i> Coração e Chimarrão
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+                
+            </div>
+            <!-- end main content-->
+
+        </div>
+        <!-- END layout-wrapper -->
+
+        <!-- Right Sidebar -->
+        <div class="right-bar">
+            <div data-simplebar class="h-100">
+                <div class="rightbar-title d-flex align-items-center px-3 py-4">
+            
+                    <h5 class="m-0 me-2">Settings</h5>
+
+                    <a href="javascript:void(0);" class="right-bar-toggle ms-auto">
+                        <i class="mdi mdi-close noti-icon"></i>
+                    </a>
+                </div>
+
+                <!-- Settings -->
+                <hr class="mt-0" />
+                <h6 class="text-center mb-0">Choose Layouts</h6>
+
+                <div class="p-4">
+                    <div class="mb-2">
+                        <img src="assets/images/layouts/layout-1.jpg" class="img-fluid img-thumbnail" alt="layout-1">
+                    </div>
+
+                    <div class="form-check form-switch mb-3">
+                        <input class="form-check-input theme-choice" type="checkbox" id="light-mode-switch" checked>
+                        <label class="form-check-label" for="light-mode-switch">Light Mode</label>
+                    </div>
+    
+                    <div class="mb-2">
+                        <img src="assets/images/layouts/layout-2.jpg" class="img-fluid img-thumbnail" alt="layout-2">
+                    </div>
+                    <div class="form-check form-switch mb-3">
+                        <input class="form-check-input theme-choice" type="checkbox" id="dark-mode-switch" data-bsStyle="assets/css/bootstrap-dark.min.css" data-appStyle="assets/css/app-dark.min.html">
+                        <label class="form-check-label" for="dark-mode-switch">Dark Mode</label>
+                    </div>
+    
+                    <div class="mb-2">
+                        <img src="assets/images/layouts/layout-3.jpg" class="img-fluid img-thumbnail" alt="layout-3">
+                    </div>
+                    <div class="form-check form-switch mb-5">
+                        <input class="form-check-input theme-choice" type="checkbox" id="rtl-mode-switch" data-appStyle="assets/css/app-rtl.min.css">
+                        <label class="form-check-label" for="rtl-mode-switch">RTL Mode</label>
+                    </div>
+
+            
+                </div>
+
+            </div> <!-- end slimscroll-menu-->
+        </div>
+        <!-- /Right-bar -->
+
+        <!-- Right bar overlay-->
+        <div class="rightbar-overlay"></div>
+
+        <!-- JAVASCRIPT -->
+        <script src="assets/libs/jquery/jquery.min.js"></script>
+        <script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="assets/libs/metismenu/metisMenu.min.js"></script>
+        <script src="assets/libs/simplebar/simplebar.min.js"></script>
+        <script src="assets/libs/node-waves/waves.min.js"></script>
+
+        <!-- apexcharts -->
+        <script src="assets/libs/apexcharts/apexcharts.min.js"></script>
+
+        <!-- Vector map-->
+        <script src="assets/libs/jsvectormap/js/jsvectormap.min.js"></script>
+        <script src="assets/libs/jsvectormap/maps/world-merc.js"></script>
+
+        <!-- echarts js -->
+        <script src="assets/libs/echarts/echarts.min.js"></script>
+
+        <script src="assets/js/pages/dashboard.init.js"></script>
+
+        <!-- App js -->
+        <script src="assets/js/app.js"></script>
+    </body>
+
+
+<!-- Mirrored from themesdesign.in/reactly/layouts/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 11 Jul 2023 16:14:06 GMT -->
+</html>
--- index.php
+++ index.php
@@ -1,4 +1,115 @@
-<?php
+<!doctype html>
+<html lang="en">
 
-echo "Teste";
-?>
+    
+<!-- Mirrored from themesdesign.in/reactly/layouts/auth-login.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 11 Jul 2023 16:15:12 GMT -->
+<head>
+        
+        <meta charset="utf-8" />
+        <title>Login | ReciclaTche</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+        <meta content="Themesdesign" name="author" />
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="assets/images/favicon.ico">
+
+        <!-- Layout Js -->
+        <script src="assets/js/layout.js"></script>
+
+        <!-- Bootstrap Css -->
+        <link href="assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
+        <!-- Icons Css -->
+        <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+        <!-- App Css-->
+        <link href="assets/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
+
+    </head>
+
+    <body>
+        <div class="auth-maintenance d-flex align-items-center min-vh-100">
+            <div class="bg-overlay bg-light"></div>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-xl-9 col-sm-12">
+                        <div>
+                            <div class="text-center mb-4">
+                                <a href="index.html" class="">
+                                    <img src="assets/images/logo-dark.png" alt="" height="25" class="auth-logo logo-dark mx-auto">
+                                    <img src="assets/images/logo-light.png" alt="" height="25" class="auth-logo logo-light mx-auto">
+                                </a>
+                            </div>
+                            <div class="row justify-content-center align-items-center pt-4">
+                                <div class="col-lg-6 col-md-8">
+                                   <div class="card">
+                                        <div class="card-body">
+                                            <div class="p-3">
+                                              <div class="text-center mt-1">
+                                                <h4 class="font-size-18">Bem Vindo!</h4>
+                                                <p class="text-muted">Coloque suas informações para realizar o login</p>
+                                              </div>
+
+                                              <form action="https://themesdesign.in/reactly/layouts/index.html" class="auth-input">
+                                                <div class="mb-2">
+                                                    <label for="username" class="form-label">Email</label>
+                                                    <input type="text" class="form-control" id="username" placeholder="Digite seu Email">
+                                                </div>
+                                        
+                                                <div class="mb-3">
+                                                    <label class="form-label" for="password-input">Senha</label>
+                                                    <input type="password" class="form-control" placeholder="Digite sua Senha">
+                                                </div>
+                                        
+                                                <div class="form-check">
+                                                    <input class="form-check-input" type="checkbox" value="" id="auth-remember-check">
+                                                    <label class="form-check-label" for="auth-remember-check">Lembrar</label>
+                                                </div>
+                                        
+                                                <div class="mt-4">
+                                                    <button class="btn btn-primary w-100" type="submit">Entrar</button>
+                                                </div>
+                                        
+                                                <div class="mt-4 pt-2 text-center">
+                                                    <div class="signin-other-title">
+                                                        <h5 class="font-size-14 mb-4 title">Sign In with</h5>
+                                                    </div>
+                                                    <!--<div class="pt-2 hstack gap-2 justify-content-center">
+                                                        <button type="button" class="btn btn-primary btn-sm"><i class="ri-facebook-fill font-size-16"></i></button>
+                                                        <button type="button" class="btn btn-danger btn-sm"><i class="ri-google-fill font-size-16"></i></button>
+                                                        <button type="button" class="btn btn-dark btn-sm"><i class="ri-github-fill font-size-16"></i></button>
+                                                        <button type="button" class="btn btn-info btn-sm"><i class="ri-twitter-fill font-size-16"></i></button>
+                                                    </div>-->
+                                                </div>
+                                            </form>
+                                            </div>
+                                        
+                                            <div class="mt-4 text-center">
+                                                <p>Quer conhecer nossas tecnologias ? <a href="#" class="fw-medium text-primary"> Entre em Contato </a> </p>
+                                                <p>© <script>document.write(new Date().getFullYear())</script> Reactly. Crafted with <i class="mdi mdi-heart text-danger"></i> o Coração e Chimarrão</p>
+                                            </div>
+                                        </div>
+                                   </div>
+                                </div>
+                            </div>
+                            <!-- end row -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <!-- JAVASCRIPT -->
+        <script src="assets/libs/jquery/jquery.min.js"></script>
+        <script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="assets/libs/metismenu/metisMenu.min.js"></script>
+        <script src="assets/libs/simplebar/simplebar.min.js"></script>
+        <script src="assets/libs/node-waves/waves.min.js"></script>
+
+        <!-- apexcharts -->
+        <script src="assets/libs/apexcharts/apexcharts.min.js"></script>
+
+        <script src="assets/js/app.js"></script>
+
+    </body>
+
+<!-- Mirrored from themesdesign.in/reactly/layouts/auth-login.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 11 Jul 2023 16:15:12 GMT -->
+</html>

8ce902f60d3ae648845533a9453d47c5516f640f
Rafael Gomes <rafael.degomes@gmail.com>
2023-08-29 20:45:04 -0300

    12312

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <head>
         
         <meta charset="utf-8" />
-        <title>Login 2 | ReciclaTche</title>
+        <title>Login 3 | ReciclaTche</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
         <meta content="Themesdesign" name="author" />

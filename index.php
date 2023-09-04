b64376cba33db008d8861984087b9ccc0382e11e
alcantaragiovanna <giovanna.alcantara@reciclatche.com.br>
2023-09-04 18:23:46 -0300

    teste

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
         
         <meta charset="utf-8" />
 
-        <title>Login 3 | ReciclaTche</title>
+        <title>Login | ReciclaTche</title>
 
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />

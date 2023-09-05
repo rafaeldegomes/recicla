dc44db323e643fcaa2b24e3d5d42318f5e6c3fad
Rafael Gomes <rafael.degomes@gmail.com>
2023-09-05 18:21:17 -0300

    retorno banco desenvolvimento

--- controller/conecta.php
+++ controller/conecta.php
@@ -1,14 +1,14 @@
 <?php
-// Conex達o com o banco de dados
+// Conexão com o banco de dados
 $servername = "localhost";
-$username = "recic443_reciclatche";
-$password = "reciclatche2023";
-$dbname = "recic443_reciclatche";
+$username = "root";
+$password = "";
+$dbname = "reciclatche";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
 if (!$conn) {
-    die("Conex達o falhou: " . mysqli_connect_error());
+    die("Conexão falhou: " . mysqli_connect_error());
 }
 
 
